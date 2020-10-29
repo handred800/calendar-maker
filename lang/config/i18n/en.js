517e7c5ab181f2cb367a772414eaa0e87adcb522
handred800 <handred800@gmail.com>
2020-10-29 18:37:34 +0800

    新增vue i18n 套件

---
+++ lang/config/i18n/en.js
@@ -0,0 +1,32 @@
+export const locale = {
+  NAME: {
+    UPLOAD: 'upload',
+    SETTING: 'setting',
+    TYPE: 'type',
+    IMAGE: 'image',
+    BACKGROUND_COLOR: 'background color',
+    SPACING: 'spacing',
+    COLOR: 'color',
+    FONT: 'font',
+    FONT_SIZE: 'fontSize',
+    ALIGN: 'text align',
+    ALIGN_DIRECTION: ['left','center','right','top','bottom'],
+    OUTPUT_SIZE: 'output size',
+    TIME: 'time',
+    DATE: 'date',
+    YEAR: 'year',
+    MONTH: 'month',
+    WEEK: 'week',
+    WEEKDAY: 'weekday',
+    WEEKEND: 'weekend',
+  },
+  LANG: {
+    EN: 'en',
+    CH: 'ch',
+    NUMBER: 'num',
+  },
+  tips: {
+    SEARCH_KEYWORD_TIPS: 'pick a random photo using keywords',
+    SEARCH_SPCIFIC_TIPS: 'paste unspalsh photo url here'
+  }
+}
